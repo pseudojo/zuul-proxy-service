ebb87dce7be298d927468a9cdaa0881b13109e7b
비디 조현구 <hgcho@bluedigm.com>
2018-01-29 14:12:17 +0900

    First commit for Zuul proxy service.

---
+++ src/main/java/com/example/zuulproxyservice/ZuulProxyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.zuulproxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyServiceApplication.class, args);
+	}
+}
